dbeb73cf0f5a5deae60472bdef77218ce42b0314
suheyb11 <suheyb0102@gmail.com>
2022-07-01 02:18:12 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,5 @@
-#include <stdio.h>
-
-
-
+#include <stdio.h
 /**
- *
  *  * main - Entry point
  *
  *   *
@@ -11,13 +7,8 @@
  *    * Return: Always 0 (Success)
  *
  *     */
-
 int main(void)
-
 {
-
 		printf("with proper grammar, but the outcome is a piece of art,\n");
-
 			return (0);
-
 }
