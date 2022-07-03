54e7fe91998ec64ee8f247f69b3773182a4478e5
suheyb yusuf bashir <107487547+suheyb11@users.noreply.github.com>
2022-07-03 12:09:39 -0700

    Create 5-print_numbers.c
    10 digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	while(i<10)
+	{
+		printf("%d",i);
+		i++;
+	}
+
+	return (0);
+}
