97046da5bc21e5845adc604afaf35d627b934c17
suheyb11 <suheyb0102@gmail.com>
2022-07-05 14:21:24 -0700

    put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,13 @@
-#include <main.h>
 #include <stdlib.h>
 #include <stdio.h>
-int main.h(void)
+int main(void)
 {
 	char *sh="suheyb";
 	while(*sh)
 	{
-	_putchar(*sh);
+	putchar(*sh);
 	sh++;
 	}
-	_putchar('\n');
+	putchar('\n');
 	return (0);
 }
