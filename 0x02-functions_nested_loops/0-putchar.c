552c60ffec2da48f0a067783f748ad2a4b21c7c7
suheyb yusuf bashir <107487547+suheyb11@users.noreply.github.com>
2022-07-06 12:27:26 -0700

    Update 0-putchar.c
    put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,21 @@
 #include "main.h"
+
 /**
- * main
+ *main - prints "_putchar"
  *
- * return
+ * Return: On success 1.
  */
 int main(void)
 {
-	char s[]="_putchar";
-	int i=0;
-	while(s[i] != '\0')
+	char s[] = "_putchar";
+	int i = 0;
+
+	while (s[i] != '\0')
 	{
 		_putchar(s[i]);
 		i++;
 	}
 	_putchar(10);
+
 	return (0);
 }
