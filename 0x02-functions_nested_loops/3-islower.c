7555222ef9cca14052098954cbc448b5fc4bde5a
suheyb11 <suheyb0102@gmail.com>
2022-07-06 13:21:57 -0700

    slw

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,8 +5,7 @@
  *   * @c: integer representing ascii value of character
  *
  *    * Return: 1 if it is lowercase or 0 otherwise
- *
- *     */
+ */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 123)
