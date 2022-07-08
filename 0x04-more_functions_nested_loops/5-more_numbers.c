b5305ff98b44e02413b027d9f93922732d3871b2
suheyb11 <suheyb0102@gmail.com>
2022-07-08 13:13:14 -0700

    main

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,10 +1,18 @@
 #include "main.h"
 
 /*
- *
- *
+ * more_numbers->more_numbers
  */
 
 void more_numbers(void)
 {
+	int x,y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 15; y++)
+			_putchar(y + '0');
+	}
+	_putchar('\n');
+}
 
