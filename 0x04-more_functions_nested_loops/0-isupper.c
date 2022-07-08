87c13a3fc67f8bb7b25a5dff43e2d180e3e7e93a
suheyb11 <suheyb0102@gmail.com>
2022-07-08 03:11:22 -0700

    main

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ *  * main - check the code
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ */
+
+int _isdigit(int c)
+{
+	if(c='A' && c <= 'Z')
+	{
+		return (0);
+	}
+	else 
+	{
+		return(1);
+	}
+}
