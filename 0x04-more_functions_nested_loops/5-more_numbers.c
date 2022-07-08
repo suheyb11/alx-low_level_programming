9d9f7eea38457c8f4bd39dd2eed803927b920c99
suheyb yusuf bashir <107487547+suheyb11@users.noreply.github.com>
2022-07-08 14:11:35 -0700

    Update 5-more_numbers.c
    commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,27 +2,28 @@
 
 /**
  * more_numbers - prints numbers from 0 to 14 ten times
+ *
+ *
  * Return: void
  */
-
 void more_numbers(void)
 {
-	int x, y;
+	int i = 0, a, b;
 
-	for (x = 0; x < 10; x++)
+	while (i < 10)
 	{
-<<<<<<< HEAD
-		for (y = 0; y < 15; y++)
+		a = 0;
+		while (a < 15)
 		{
-			if (y >= 10)
-				_putchar((y / 10) + '0');
-		        _putchar((y % 10) + '0');
+			b = a;
+			if (a > 9)
+				b = a / 10;
+			_putchar('0' + b);
+			if (a > 9)
+				_putchar('0' + a % 10);
+			a++;
 		}
 		_putchar('\n');
-=======
-		for (y = 0; y < 14; y++)
-			_putchar(y + '0');
->>>>>>> 0d4afd6161e32b15b2d2cc795bf39da5b82712d5
+		i++;
 	}
-	
 }
