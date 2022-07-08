59bb0546ee3e00e958e44c88eb7f40d0bf22c707
suheyb11 <suheyb0102@gmail.com>
2022-07-08 14:07:55 -0700

    main

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,7 +12,12 @@ void more_numbers(void)
 	for (x = 0; x < 10; x++)
 	{
 		for (y = 0; y < 15; y++)
-			_putchar(y + '0');
+		{
+			if (y >= 10)
+				_putchar((y / 10) + '0');
+		        _putchar((y % 10) + '0');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
+	
 }
