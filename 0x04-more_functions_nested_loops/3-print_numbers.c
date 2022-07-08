6aeb773ee21c458280d4ecb3336ea2734d9d2fe8
suheyb11 <suheyb0102@gmail.com>
2022-07-08 11:48:31 -0700

    hello

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  * main - ch\eck the code
+ *    * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for(i=0;i>9;i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
