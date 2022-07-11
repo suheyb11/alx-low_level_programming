04056164bc6942608f198c8fab5474687d1c4ff1
suheyb11 <suheyb0102@gmail.com>
2022-07-11 12:16:48 -0700

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,16 +4,14 @@
  * _puts - prints to string
  * @str: apointeto an int that be changed
  *
- * return: void which means our answer is cotrrect
+ * Return: void which means our answer is cotrrect
  */
 
 void _puts(char *str)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
+	for (; *str != '\0'; *str++)
 	{
-		_putchar(s[i]);
+		_putchar(*str);
 	}
 	_putchar('\n');
 }
