588cb55556b232bbf4ce8c4995e566f34fc4c5b2
suheyb11 <suheyb0102@gmail.com>
2022-07-11 14:14:42 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * rev_string - is funtion that reverse string
+ * @s: is pointer
+ */
+
+void rev_string(char *s)
+{
+	int l;
+
+	l = strlen(s);
+	for (l = l - 1; l >= 0; l--)
+	{
+		_putchar(s[l]);
+	}
+	_putchar('\n');
+}
