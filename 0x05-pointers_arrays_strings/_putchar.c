301d3cefbd5206d09476f8581343f800cef24087
suheyb11 <suheyb0102@gmail.com>
2022-07-12 06:57:29 -0700

    main

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,6 +1,11 @@
 #include <unistd.h>
 
+/**
+ * _putchar - write the chracter c to stdout
+ * @c: the chracter to print
+ */
+
 int _putchar(char c)
 {
-	return ( Write(1, &c, 1));
+	return (Write(1, &c, 1));
 }
