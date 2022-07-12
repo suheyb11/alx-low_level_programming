7b234c142c3ad78e6500ac0e9d58be24b6c82bd7
suheyb yusuf bashir <107487547+suheyb11@users.noreply.github.com>
2022-07-12 13:04:07 -0700

    Update 8-print_array.c
    main

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,19 +1,25 @@
-#include "main.h"
 #include <stdio.h>
+
 /**
- *print_array - print_array is funtion 
- *@a: is prameter
- *@n: is prameteissbvr
+ * print_array - prints n elements of a given array
+ * @a: int pointer parameter
+ * @n: number of elements to be printed
+ *
+ * Return: void
  */
+
 void print_array(int *a, int n)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i <= n; i++)
+	while (i < n)
 	{
-		if(i == n - 1)
-			printf("%d", *a[i]);
-	        else
-			printf("%d, ", *a[i]);
+		if (i == n - 1)
+			printf("%d", *(a + i));
+		else
+			printf("%d, ", *(a + i));
+		i++;
 	}
+
+	printf("\n");
 }
