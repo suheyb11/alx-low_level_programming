4ecca6e8c9ad934d8e08e63af723043b6982850c
suheyb11 <suheyb0102@gmail.com>
2022-07-13 14:47:41 -0700

    main

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,21 +4,18 @@
  * _strcmp - > is funtion that comp
  * @s1: is one prm
  * @s2: is seconf prm
+ * Return: is void not corct
  */
 int _strcmp(char *s1, char *s2)
 {
-	int f, i;
+	int i;
 
-	f = 0;
-	for (i = 0;s1[i] != '\0' || s2[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		if (s1[i] = s2[i])
+		if (s1[i] != s2[i])
 		{
-			flag = 1;
 			break;
 		}
 	}
-	if (flag == 1)
-		_putchar("not same");
+	return (s1[i] - s2[i]);
 }
-
