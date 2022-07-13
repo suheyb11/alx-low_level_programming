3dc9a5db56e71317fcb4e1718b43d8d6a551ec6d
suheyb11 <suheyb0102@gmail.com>
2022-07-13 11:40:02 -0700

    main

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,15 +2,21 @@
 #include <string.h>
 
 /**
- * _strcat - _strcat is funtion
+ * _strcat ->  _strcat is funtion
  * @dest: is prameter
- * @c: is prameter
+ * @src: is prameter
+ * Return: is void becouse our data is correct
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int p;
+	int p, i;
 
-	p = strcat(dest, src);
+	p = 0;
+	while (dest[p])
+		p++;
+	for (i = 0; src[i] ; i++)
+		dest[p] = src[i];
+	return (dest);
+
 }
-
