28ee9231e56366ff6fcc830b7e66a40af6578483
suheyb yusuf bashir <107487547+suheyb11@users.noreply.github.com>
2022-07-14 00:41:30 +0300

    Update 3-strcmp.c
    min

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,6 +9,24 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	strncmp(s1, s2):
-	return (s1);
+	int i = 0;
+    
+      /* ----------------------------------
+   	 '\0' marks the end of the string !
+   	 ---------------------------------- */
+      while ( s2[i] != '\0' && s1[i] != 0 )
+      {
+   	 if ( s1[i] != s2[i] )          // If s1 and s2 differ in char i
+   	    break;                      // exit while loop !
+    
+   	 i++;
+      }
+    
+      /* ----------------------------------------------------------------
+   	 If we reach here, then:
+           1. one or both of s1 and s2 are exhausted !
+	   2. character position i in s1 and s2 are different
+   	 ---------------------------------------------------------------- */
+    
+      return s1[i] - s2[i]; 
 }
