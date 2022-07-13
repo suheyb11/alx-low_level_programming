41f26d8aaef8da8bf2fa40115671b531bf02915d
suheyb11 <suheyb0102@gmail.com>
2022-07-13 08:12:48 -0700

    main

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - _strcat is funtion
+ * @dest: is prameter
+ * @c: is prameter
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int p;
+
+	p = strcat(dest, src);
+}
+
