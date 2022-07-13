8548a55dffde6a6827c318a83cbaa39ce1a50329
suheyb yusuf bashir <107487547+suheyb11@users.noreply.github.com>
2022-07-14 00:39:16 +0300

    Update 3-strcmp.c
    main

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,24 +1,14 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * _strcmp - > is funtion that comp
  * @s1: is one prm
  * @s2: is seconf prm
+ * Return: is voud not do
  */
 int _strcmp(char *s1, char *s2)
 {
-	int f, i;
-
-	f = 0;
-	for (i = 0;s1[i] != '\0' || s2[i] != '\0'; i++)
-	{
-		if (s1[i] = s2[i])
-		{
-			flag = 1;
-			break;
-		}
-	}
-	if (flag == 1)
-		_putchar("not same");
+	strncmp(s1, s2):
+	return (s1);
 }
-
