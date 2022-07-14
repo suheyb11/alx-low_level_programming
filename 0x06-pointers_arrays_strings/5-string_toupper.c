7176f5b0096f20c6903189130d322836e918bace
suheyb yusuf bashir <107487547+suheyb11@users.noreply.github.com>
2022-07-14 12:33:51 -0700

    Update 5-string_toupper.c
    main

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,24 +1,20 @@
 #include "main.h"
 
 /**
- * string_toupper - > is fiuntion tahy
- * @s: is prameter
-<<<<<<< HEAD
- * Return: is void becouse our data is correct
-=======
- * Return: is void becouse our data is correct 
->>>>>>> fed78403340ca2e82eecc01ba08a59e77f2c179e
+ *string_toupper - converts a given string to upper case.
+ *@str: parameter
+ *Return: char *
  */
-char *string_toupper(char *s)
-{
-	int x;
 
-	for (x = 0; s[x] != '\0'; x++)
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i++])
 	{
-		if (s[x] > 96 && s[x] < 123)
-		{
-			s[x] -= 32;
-		}
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
 	}
-	return (s);
+
+	return (str);
 }
