2e2e5fbbcc2f54c8269b3547d6e267c41115c766
suheyb11 <suheyb0102@gmail.com>
2022-07-14 00:06:55 -0700

    main

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * reverse_array - > reverse_array is funtion
+ * @a: is one ormn
+ * @n: teo prm
+ * Return: is void not crct
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	for (i = n; i < n - 1; i--)
+	{
+		return (a[i]);
+	}
+}
