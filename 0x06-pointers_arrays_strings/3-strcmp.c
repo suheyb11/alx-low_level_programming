6a84c81900b02adb0cdab1a4f2efe69113637b93
suheyb11 <suheyb0102@gmail.com>
2022-07-13 23:47:53 -0700

    your commit message

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,10 +11,11 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; (s1[i] != '\0' && s2[i] != '\0') && s[i] == s2[i]; i++)
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	
 	if (s1[i] == s2[i])
 	{
 		return (0);
 	}
-	return (s1[i] - s2[i]);
+        return (s1[i] - s2[i]);
 }
