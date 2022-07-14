2e76daa4d0208473b70a986659c119dfd6b109f5
suheyb yusuf bashir <107487547+suheyb11@users.noreply.github.com>
2022-07-14 09:41:29 +0300

    Update 3-strcmp.c
    main

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,12 +9,19 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-
-	for (i = 0; (s1[i] != '\0' && s2[i] != '\0') && s[i] == s2[i]; i++)
-	if (s1[i] == s2[i])
+	while( ( s1 != '\0' && s2 != '\0' ) && s1 == s2 )
 	{
-		return (0);
+		s1++;
+		s2++;
 	}
-	return (s1[i] - s2[i]);
+	
+	if(s1 == s2)
+	{
+		return 0; // strings are identical
+	}
+
+	else
+	{
+		return s1 - s2;
+	}
 }
