95dde31033758cd10b1c2e3d250ccff35de3305b
suheyb11 <suheyb0102@gmail.com>
2022-07-14 11:53:29 -0700

    main

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - > is fiuntion tahy
+ * @s: is prnrm
+ * Return: is void not 
+ */
+char *string_toupper(char *s)
+{
+	int x;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		if (s[x] > 96 && s[x] < 123)
+		{
+			s[x] -= 32;
+		}
+	}
+	return (s);
+}
