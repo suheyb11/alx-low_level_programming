6a518ad6f30bca387f208a8e31ff4f5e70a9aea7
suheyb11 <suheyb0102@gmail.com>
2022-07-13 23:25:18 -0700

    main

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,6 @@
  * _strcmp - > is funtion that comp
  * @s1: is one prm
  * @s2: is seconf prm
-<<<<<<< HEAD
  * Return: is void not corct
  */
 int _strcmp(char *s1, char *s2)
@@ -14,36 +13,10 @@ int _strcmp(char *s1, char *s2)
 
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		if (s1[i] != s2[i])
+		if (s1[i] == s2[i])
 		{
 			break;
 		}
 	}
 	return (s1[i] - s2[i]);
-=======
- * Return: is voud not do
- */
-int _strcmp(char *s1, char *s2)
-{
-	int i = 0;
-    
-      /* ----------------------------------
-   	 '\0' marks the end of the string !
-   	 ---------------------------------- */
-      while ( s2[i] != '\0' && s1[i] != 0 )
-      {
-   	 if ( s1[i] != s2[i] )          // If s1 and s2 differ in char i
-   	    break;                      // exit while loop !
-    
-   	 i++;
-      }
-    
-      /* ----------------------------------------------------------------
-   	 If we reach here, then:
-           1. one or both of s1 and s2 are exhausted !
-	   2. character position i in s1 and s2 are different
-   	 ---------------------------------------------------------------- */
-    
-      return s1[i] - s2[i]; 
->>>>>>> 28ee9231e56366ff6fcc830b7e66a40af6578483
 }
