aa4dc5694d5ce3906c84a6111d0d0fd534c170ec
suheyb yusuf bashir <107487547+suheyb11@users.noreply.github.com>
2022-09-13 12:59:46 +0300

    Create 5-sign.c
    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: positive, zero or negative integer
+ * Return: 1 if it's positive, 0 if it's negative and -1 if it's negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
+Footer
+© 2022 GitHub, Inc.
