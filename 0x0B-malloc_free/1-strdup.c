16dbc72d6e9a1dedce84b4a68a85a3a683b3db61
suheyb yusuf bashir <107487547+suheyb11@users.noreply.github.com>
2022-10-03 17:40:22 +0300

    Update 1-strdup.c
    hi

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,30 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+
 /**
- * _strdup - duplicate to new memory space location
- * @str: char
- * Return: 0
+ * string_nconcat - function to concatnate strings with n bytes
+ * @s1: destination for concatnation
+ * @s2: source of string
+ * @n: int type for size of byte
+ * Return: pointer to new memory allocated
  */
-char *_strdup(char *str)
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i, end;
-	char *array;
+	int count, count1;
+	int sign = n;
+	char *ptr;
+	int len1, len2;
 
-	if (str == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-	for (end = 0; end <= *str; end++)
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+		;
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+		;
+
+	if (sign >= len2)
 	{
+		sign = len2;
+		ptr = malloc(sizeof(char) * (len1 + len2 + 1));
 	}
-
-	end += 1;
-	array = malloc(sizeof(char) * end);
-
-	for (i = 0; i < end; i++)
-		array[i] = str[i];
-
-	if (array == NULL)
+	else
+		ptr = malloc(sizeof(char) * (len1 + n + 1));
+	if (ptr == NULL)
 		return (NULL);
-	return (array);
+	for (count = 0; count < len1; count++)
+	{
+		ptr[count] = s1[count];
+	}
+	for (count1 = 0; count1 < sign; count1++)
+	{
+		ptr[count++] = s2[count1];
+	}
+	ptr[count++] = '\0';
+	return (ptr);
 }
